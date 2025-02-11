622864d19718c3283a1f54967e637a1335e3ada8
Prathmesh Santosh Biradar <psbiradar948@gmail.com>
2025-02-11 11:27:45 +0530

    Update Predicate1.java

--- src/PredicatePrograms/Predicate1.java
+++ src/PredicatePrograms/Predicate1.java
@@ -1,48 +1,48 @@
-package PredicatePrograms;
+package predicatePrograms;
 
 import java.util.function.Predicate;
 
-public class Predicate1 {
+public class Predicate {
     public static void main(String[] args) {
         Predicate<Integer> p1 = i -> i%2 == 0; // predicate function
         System.out.println( p1.test(34)); // testing number is even or not.
 
-        String[]s ={"Yash","Abhishek","Om"};
-        Predicate<String>p2 = i -> i.length()>0; //testing the length of string greater than 0.
+        String[]name ={"Yash","Abhishek","Om"};
+        Predicate<String>newName = i -> i.length()>0; //testing the length of string greater than 0.
 
-        for(String s1 : s)
+        for(String names : name)
         {
-            if(p2.test(s1))
+            if(newName.test(names))
             {
-                System.out.println(s1); // printing the String.
+                System.out.println(names); // printing the String.
             }
         }
 
-        int[] i ={13,35,44,76,32,76,44,33,55,34,67,36,43};
-        Predicate<Integer> p3 = x -> x%2==0;
-        Predicate<Integer> p4 = y -> y>20;
+        int[] numbers ={13,35,44,76,32,76,44,33,55,34,67,36,43};
+        Predicate<Integer> evenNumber = x -> x%2==0;
+        Predicate<Integer> numberGreater = y -> y>20;
         System.out.println("Number is even and greater than 20");
-        for(int z : i)
+        for(int number : numbers)
         {
-            if(p3.and(p4).test(z)) // and predicate joining
-                System.out.print(z+" ");
+            if(evenNumbers.and(numberGreater).test(number)) // and predicate joining
+                System.out.print(number+" ");
         }
         System.out.println();
         System.out.println("Number is even or greater than 20");
-        for(int z: i)
+        for(int number: numbers)
         {
-            if(p3.or(p4).test(z)) // or predicate joining
+            if(evenNumber.or(numberGreater).test(number)) // or predicate joining
             {
-                System.out.print(z+" ");
+                System.out.print(number+" ");
             }
         }
         System.out.println();
         System.out.println(" Odd numbers are: ");
-        for(int z:i)
+        for(int number:numbers)
         {
-            if(p3.negate().test(z))
+            if(evenNumber.negate().test(number))
             {
-                System.out.print(z+" ");
+                System.out.print(number+" ");
             }
         }
 
